266c82a904f3b4817266a269d883e27248261f78
bilal-abraham <bilal.abraham.717@gmail.com>
2021-12-02 22:41:30 -0800

    2021 Day 3 Complete

--- 2021/Day3/part1.js
+++ 2021/Day3/part1.js
@@ -1,3 +1,24 @@
 const fs = require("fs");
 
 const lines = fs.readFileSync('input.txt', { encoding: 'utf-8' }).split('\n').filter(x => x);
+
+
+let gamma = '';
+let epsilon = '';
+let powerConsumption;
+
+for (let i = 0; i < lines[0].length; i++) {
+    let ones = lines
+        .map((number) => number[i] === '1')
+        .filter(x => x);
+    if (ones.length >= lines.length / 2) {
+        gamma += '1';
+        epsilon += '0';
+    } else {
+        gamma += '0';
+        epsilon += '1';
+    }
+}
+
+powerConsumption = parseInt(gamma, 2) * parseInt(epsilon, 2);
+console.log(powerConsumption)
--- 2021/Day3/part2.js
+++ 2021/Day3/part2.js
@@ -1,3 +1,24 @@
 const fs = require("fs");
 
 const lines = fs.readFileSync('input.txt', { encoding: 'utf-8' }).split('\n').filter(x => x);
+
+
+let oxySearch = lines;
+let scrubSearch = lines;
+let lifeSupportRating;
+let i = 0;
+
+while (oxySearch.length > 1 || scrubSearch.length > 1) {
+    if (oxySearch.length > 1) {
+        let ones = oxySearch.map((number) => number[i] === '1').filter((bit) => bit);
+        oxySearch = oxySearch.filter((number) => number[i] === (ones.length >= oxySearch.length / 2 ? '1' : '0'));
+    }
+    if (scrubSearch.length > 1) {
+        let ones = scrubSearch.map((number) => number[i] === '1').filter((bit) => bit);
+        scrubSearch = scrubSearch.filter((number) => number[i] === (ones.length >= scrubSearch.length / 2 ? '0' : '1'));
+    }
+    i++;
+}
+
+lifeSupportRating = parseInt(oxySearch[0], 2) * parseInt(scrubSearch[0], 2);
+console.log(lifeSupportRating)
